7e1fe751dc523001b5ac526d63ebf8218aff24e8
geryborisova <geryborisova@abv.bg>
2019-03-04 13:11:07 +0200

    some changes

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,2 +1,39 @@
-// import React from 'react';
-// import classes from './Footer.module.scss';
+import React from 'react'; 
+import classes from './Footer.module.scss';
+
+class FooterComponent extends React.Component {
+    render(){
+        return(
+            <footer className={classes.Footer}>
+            <div className={classes.Column}>
+            <h3>About us</h3>
+            <h3>Careers</h3>
+           
+            
+            <h3>Terms</h3>
+            <h3>Privacy</h3>
+           
+            <h3>Authors</h3>
+            <h3>Advertise</h3>
+          
+            <h3>Authors & ads blog</h3>
+           
+            <h3>API</h3>
+            
+            </div>
+
+
+           <div className={classes.rights}>
+            <p>© 2019 Goodreads, Inc.</p>
+            </div>
+         
+            
+            
+            </footer>
+        )
+    }
+
+
+}
+ 
+export default FooterComponent;
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -4,17 +4,15 @@ import classes from './Header.module.scss';
 import Button from '../UI/Button/Button.js';
 import background from '../assets/images/background-book.jpg';
 import backgroundWords from '../assets/images/title.png';
-import Footer from '../assets/Footer/Footer';
+import Footer from '../Footer/Footer'
 
 
 
-
-
-class LoginComponent extends React.Component {
+class HeaderComponent extends React.Component {
 
     render() {
+        const myStyle={backgroundColor: 'rgb(255, 213, 109)',width:'90px',fontSize:'1em',border:'none', height:'40px', color: '#382110'};
         return (
-           <React.Fragment>
             <header>
                 <div className={classes.Header}>
                 <img className={classes.Logo} src={logo} alt="logo" />
@@ -23,19 +21,35 @@ class LoginComponent extends React.Component {
                     <input type="email" placeholder="Email address" />
                     <input type="password" placeholder="Password" />
                     <Button title="Sign in"></Button>
+                    </div>
+                    
+                    
                 </div>
+                < div >
+                <div >
+                    <img className={classes.backgroundWords} src={backgroundWords} alt='Meet your next favourite book'>
+                    </img>
+                    <div className={classes.Register}>
+                    <p className={classes.regP}>
+                    New here? Create a free account!
+                    </p>
+                    <input type="text" placeholder="Name" />
+                    <input type="email" placeholder="Email address" />
+                    <input type="password" placeholder="Password" />
+                   
+                    <Button style={myStyle} title="Sign up" width='100px'></Button>
+                    </div>
                 </div>
+                <img className={classes.background} src={background}  height='500px' alt="logo" />
+                </div>
+                <Footer/>
                 </header>
-
-              <Footer/>
-              
-                </React.Fragment>
+                
            
             
-           
             
         );
     }
 }
 
-export default LoginComponent; 
+export default HeaderComponent;
